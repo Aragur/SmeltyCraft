344a6ac38e343ad2c3041c2df89116345874215e
AragurLP <aragurlp@gmail.com>
2015-03-31 22:15:29 +0200

    Testing ItemFramework

---
+++ src/main/java/net/aragurlp/smeltycraft/item/ItemTest.java
@@ -0,0 +1,10 @@
+package net.aragurlp.smeltycraft.item;
+
+public class ItemTest extends ItemSC
+{
+    public ItemTest()
+    {
+        super();
+        this.setUnlocalizedName("testItem");
+    }
+}
