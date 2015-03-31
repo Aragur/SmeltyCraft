1c9e02ffa313f79c823388b5df22b916a15bf715
AragurLP <aragurlp@gmail.com>
2015-03-31 18:56:51 +0200

    Updated Reference handling

--- src/main/java/net/aragurlp/smeltycraft/SmeltyCraft.java
+++ src/main/java/net/aragurlp/smeltycraft/SmeltyCraft.java
@@ -1,15 +1,21 @@
 package net.aragurlp.smeltycraft;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.aragurlp.smeltycraft.proxy.IProxy;
+import net.aragurlp.smeltycraft.reference.Reference;
 
-@Mod(modid = "SmeltyCraft", name = "Smelty Craft", version = "1.7.10-1.0")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class SmeltyCraft {
 
-    @Mod.Instance("SmeltyCraft")
+    @Mod.Instance(Reference.MOD_ID)
     public static SmeltyCraft instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
+    public static IProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event){
