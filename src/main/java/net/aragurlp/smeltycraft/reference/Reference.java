656f033e41dc945df824bbb0ae3c59e02328175d
AragurLP <aragurlp@gmail.com>
2015-03-31 18:54:47 +0200

    First Initiation

---
+++ src/main/java/net/aragurlp/smeltycraft/reference/Reference.java
@@ -0,0 +1,12 @@
+package net.aragurlp.smeltycraft.reference;
+
+
+public class Reference {
+
+    public static final String MOD_ID = "SmeltyCraft";
+    public static final String MOD_NAME = "Smelty Craft";
+    public static final String VERSION = "1.7.10-1.0";
+
+    public static final String CLIENT_PROXY_CLASS = "net.aragurlp.smeltycraft.proxy.ClientProxy";
+    public static final String SERVER_PROXY_CLASS = "net.aragurlp.smeltycraft.proxy.ServerProxy";
+}
