cc039e475906ec3dce9e1cf78f97faed904dd245
AragurLP <aragurlp@gmail.com>
2015-03-31 20:35:26 +0200

    Update for incoming GuiConfiguration

--- src/main/java/net/aragurlp/smeltycraft/reference/Reference.java
+++ src/main/java/net/aragurlp/smeltycraft/reference/Reference.java
@@ -9,4 +9,6 @@ public class Reference {
 
     public static final String CLIENT_PROXY_CLASS = "net.aragurlp.smeltycraft.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "net.aragurlp.smeltycraft.proxy.ServerProxy";
+
+    public static final String GUI_FACTORY_CLASS = "net.aragurlp.smeltycraft.client.gui.GuiFactory";
 }
