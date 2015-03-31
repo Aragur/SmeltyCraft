65aeba26865429a261443bbba7a5a9f3b59e377e
AragurLP <aragurlp@gmail.com>
2015-03-31 20:52:14 +0200

    Cleaned

--- src/main/java/net/aragurlp/smeltycraft/util/LogHelper.java
+++ src/main/java/net/aragurlp/smeltycraft/util/LogHelper.java
@@ -10,19 +10,29 @@ public class LogHelper {
         FMLLog.log(Reference.MOD_NAME, logLevel, String.valueOf(object));
     }
 
-    public static void all(Object object) {log(Level.ALL, object);}
-
-    public static void debug(Object object) {log(Level.DEBUG, object);}
-
-    public static void error(Object object) {log(Level.ERROR, object);}
-
+    /**
+     * A severe error that will prevent the application from continuing.
+     */
     public static void fatal(Object object) {log(Level.FATAL, object);}
 
+    /**
+     * An error in the application, possibly recoverable.
+     */
+    public static void error(Object object) {log(Level.ERROR, object);}
+
+    /**
+     * An event that might possible lead to an error.
+     */
+    public static void warn(Object object) {log(Level.WARN, object);}
+
+    /**
+     * An event for informational purposes.
+     */
     public static void info(Object object) {log(Level.INFO, object);}
 
-    public static void off(Object object) {log(Level.OFF, object);}
+    /**
+     * A general debugging event.
+     */
+    public static void debug(Object object) {log(Level.DEBUG, object);}
 
-    public static void trace(Object object) {log(Level.TRACE, object);}
-
-    public static void warn(Object object) {log(Level.WARN, object);}
 }
