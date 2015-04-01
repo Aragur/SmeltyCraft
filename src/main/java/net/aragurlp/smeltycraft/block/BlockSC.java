8d0e3b64df93eb947ae43c23a1d01ad4b7dc6599
AragurLP <aragurlp@gmail.com>
2015-04-01 11:09:07 +0200

    Generic Block Class

---
+++ src/main/java/net/aragurlp/smeltycraft/block/BlockSC.java
@@ -0,0 +1,41 @@
+package net.aragurlp.smeltycraft.block;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.aragurlp.smeltycraft.reference.Reference;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
+
+public class BlockSC extends Block
+{
+    public BlockSC(Material material)
+    {
+        super(material);
+    }
+
+    public BlockSC()
+    {
+        this(Material.rock);
+    }
+
+    @Override
+    public String getUnlocalizedName()
+    {
+        return String.format("tile.%s%s", Reference.MOD_ID.toLowerCase() + ":", getUnWrappedUnlocalizedName(super.getUnlocalizedName()));
+        //tile.modid:blockname.name
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        blockIcon = iconRegister.registerIcon(String.format("%s", getUnWrappedUnlocalizedName((this.getUnlocalizedName()))));
+    }
+
+    protected String getUnWrappedUnlocalizedName(String unlocalizedName)
+    {
+        return unlocalizedName.substring(unlocalizedName.indexOf(".") + 1);
+    }
+
+}
