a2fe0e9e372690080614e0b5fc0a5c11d21494c6
AragurLP <aragurlp@gmail.com>
2015-04-01 11:09:32 +0200

    Test Block

---
+++ src/main/java/net/aragurlp/smeltycraft/block/BlockTest.java
@@ -0,0 +1,14 @@
+package net.aragurlp.smeltycraft.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockTest extends BlockSC
+{
+    public BlockTest()
+    {
+        super(Material.iron);
+        this.setBlockName("testBlock");
+        this.setStepSound(soundTypeMetal);
+    }
+}
