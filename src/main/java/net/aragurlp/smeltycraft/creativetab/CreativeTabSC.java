5d36b5235b5eb1caf067b632738a92270b68e305
AragurLP <aragurlp@gmail.com>
2015-04-01 11:40:58 +0200

    Added Creative Tab

---
+++ src/main/java/net/aragurlp/smeltycraft/creativetab/CreativeTabSC.java
@@ -0,0 +1,18 @@
+package net.aragurlp.smeltycraft.creativetab;
+
+import net.aragurlp.smeltycraft.init.ModItems;
+import net.aragurlp.smeltycraft.reference.Reference;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class CreativeTabSC
+{
+    public static final CreativeTabs SC_TAB = new CreativeTabs(Reference.MOD_ID)
+    {
+        @Override
+        public Item getTabIconItem()
+        {
+            return ModItems.testItem;
+        }
+    };
+}
