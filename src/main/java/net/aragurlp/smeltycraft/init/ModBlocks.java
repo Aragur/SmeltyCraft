e9a3ac1082e1296170ce09bbc0b73abec7aa8076
AragurLP <aragurlp@gmail.com>
2015-04-02 22:05:34 +0200

    Added Smelting Furnace

--- src/main/java/net/aragurlp/smeltycraft/init/ModBlocks.java
+++ src/main/java/net/aragurlp/smeltycraft/init/ModBlocks.java
@@ -1,21 +1,22 @@
 package net.aragurlp.smeltycraft.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.aragurlp.smeltycraft.block.BlockMultiBlock;
-import net.aragurlp.smeltycraft.block.BlockMultiStone;
-import net.aragurlp.smeltycraft.block.BlockSC;
-import net.aragurlp.smeltycraft.block.BlockTest;
+import net.aragurlp.smeltycraft.block.*;
 import net.aragurlp.smeltycraft.reference.Reference;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
 
     public static final BlockSC testBlock = new BlockTest();
-    public static final BlockMultiBlock stoneMultiBlock= new BlockMultiStone();
+    public static final BlockContainerSC stoneMultiBlock = new BlockTestMultiBlock();
+    public static final BlockContainerSC smeltingfurnace = new BlockSmeltingFurnace(false);
+    public static final BlockContainerSC smeltingfurnace_lit = new BlockSmeltingFurnace(true);
 
     public static void init()
     {
         GameRegistry.registerBlock(testBlock, "testBlock");
         GameRegistry.registerBlock(stoneMultiBlock, "stoneMultiBlock");
+        GameRegistry.registerBlock(smeltingfurnace, "smeltingFurnace");
+        GameRegistry.registerBlock(smeltingfurnace_lit, "smeltingFurnace_lit");
     }
 }
--- src/main/java/net/aragurlp/smeltycraft/init/TileEntitys.java
+++ src/main/java/net/aragurlp/smeltycraft/init/TileEntitys.java
@@ -2,13 +2,16 @@ package net.aragurlp.smeltycraft.init;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.aragurlp.smeltycraft.reference.Reference;
+import net.aragurlp.smeltycraft.tile.TileSmeltingFurnace;
 import net.aragurlp.smeltycraft.tile.TileStoneMultiblock;
 
 public class TileEntitys {
     public static final Class tileStoneMultiblock = TileStoneMultiblock.class;
+    public static final Class tileSmeltingFurnace = TileSmeltingFurnace.class;
 
     public static void init()
     {
         GameRegistry.registerTileEntity(tileStoneMultiblock, Reference.MOD_ID.toLowerCase() + ".multiblock.stone");
+        GameRegistry.registerTileEntity(tileSmeltingFurnace, Reference.MOD_ID.toLowerCase() + ".tile.smeltingfurnace");
     }
 }
