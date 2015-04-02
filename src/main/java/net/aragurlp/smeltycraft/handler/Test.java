abe25782bf2e1c41dca2a879f32c444d33e5cd88
AragurLP <aragurlp@gmail.com>
2015-04-02 22:20:27 +0200

    Added GuiHandler

---
+++ src/main/java/net/aragurlp/smeltycraft/handler/Test.java
@@ -0,0 +1,57 @@
+package net.aragurlp.smeltycraft.handler;
+
+import cpw.mods.fml.common.network.NetworkRegistry;
+import net.aragurlp.smeltycraft.SmeltyCraft;
+import net.aragurlp.smeltycraft.client.gui.GuiSmeltingFurnace;
+import net.aragurlp.smeltycraft.reference.GUIs;
+import net.aragurlp.smeltycraft.tile.TileSmeltingFurnace;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
+
+public class Test {
+        public static void init()
+        {
+            NetworkRegistry.INSTANCE.registerGuiHandler(SmeltyCraft.instance, new GuiHandler());
+        }
+
+        @Override
+        public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+        {
+            TileEntity entity = world.getTileEntity(x, y, z);
+
+            if(entity != null)
+            {
+                switch(ID)
+                {
+                    case GUIs.SMELTING_FURNACE :
+                        if(entity instanceof TileSmeltingFurnace)
+                        {
+//                        return new ContainerSmeltingFurnace(player.inventory, (TileSmeltingFurnace) entity);
+                        }
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+        {
+            TileEntity entity = world.getTileEntity(x, y, z);
+
+            if(entity != null)
+            {
+                switch(ID)
+                {
+                    case GUIs.SMELTING_FURNACE :
+                        if(entity instanceof TileSmeltingFurnace)
+                        {
+                            return new GuiSmeltingFurnace(player.inventory, (TileSmeltingFurnace) entity);
+                        }
+                }
+            }
+            return null;
+        }
+    }
+
+}
