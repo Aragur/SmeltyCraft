953b8982907cf0745cb0d7c5bd0030ff0a70ed27
AragurLP <aragurlp@gmail.com>
2015-04-05 12:59:49 +0200

    Added needed methods

--- src/main/java/net/aragurlp/smeltycraft/tile/TileSmeltingFurnace.java
+++ src/main/java/net/aragurlp/smeltycraft/tile/TileSmeltingFurnace.java
@@ -1,11 +1,13 @@
 package net.aragurlp.smeltycraft.tile;
 
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.aragurlp.smeltycraft.block.BlockSmeltingFurnace;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.ISidedInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileSmeltingFurnace extends TileEntity implements ISidedInventory{
@@ -47,23 +49,59 @@ public class TileSmeltingFurnace extends TileEntity implements ISidedInventory{
     }
 
     @Override
-    public ItemStack getStackInSlot(int p_70301_1_) {
+    public ItemStack getStackInSlot(int i)
+    {
+        return this.slots[i];
+    }
+
+    @Override
+    public ItemStack decrStackSize(int i, int j)
+    {
+        if(this.slots[i] != null)
+        {
+            ItemStack itemStack;
+            if(this.slots[i].stackSize <= j)
+            {
+                itemStack = this.slots[i];
+                this.slots[i] = null;
+                return itemStack;
+            }
+            else
+            {
+                itemStack = this.slots[i].splitStack(j);
+
+                if(this.slots[i].stackSize == 0)
+                {
+                    this.slots[i] = null;
+                }
+
+                return itemStack;
+            }
+        }
         return null;
     }
 
     @Override
-    public ItemStack decrStackSize(int p_70298_1_, int p_70298_2_) {
+    public ItemStack getStackInSlotOnClosing(int i)
+    {
+        if(this.slots[i] != null)
+        {
+            ItemStack itemStack = this.slots[i];
+            this.slots[i] = null;
+            return itemStack;
+        }
         return null;
     }
 
     @Override
-    public ItemStack getStackInSlotOnClosing(int p_70304_1_) {
-        return null;
-    }
+    public void setInventorySlotContents(int i, ItemStack itemStack)
+    {
+        this.slots[i] = itemStack;
 
-    @Override
-    public void setInventorySlotContents(int p_70299_1_, ItemStack p_70299_2_) {
-
+        if(itemStack != null && itemStack.stackSize > this.getInventoryStackLimit())
+        {
+            itemStack.stackSize = this.getInventoryStackLimit();
+        }
     }
 
     @Override
@@ -77,13 +115,14 @@ public class TileSmeltingFurnace extends TileEntity implements ISidedInventory{
     }
 
     @Override
-    public int getInventoryStackLimit() {
-        return 0;
+    public int getInventoryStackLimit()
+    {
+        return 64;
     }
 
     @Override
-    public boolean isUseableByPlayer(EntityPlayer p_70300_1_) {
-        return false;
+    public boolean isUseableByPlayer(EntityPlayer player) {
+        return this.worldObj.getTileEntity(this.xCoord, this.yCoord, this.zCoord) != this ? false : player.getDistanceSq((double)this.xCoord + 0.5D, (double)this.yCoord + 0.5D, (double)this.zCoord + 0.5D) <= 64.0D;
     }
 
     @Override
@@ -96,9 +135,17 @@ public class TileSmeltingFurnace extends TileEntity implements ISidedInventory{
 
     }
 
+    private boolean isBurning()
+    {
+        return this.burnTime > 0;
+    }
+
     @Override
     public void updateEntity()
     {
+        boolean flag = this.burnTime > 0;
+        boolean flag1 = false;
+
         if(this.burnTime > 0)
         {
             this.burnTime--;
@@ -109,6 +156,85 @@ public class TileSmeltingFurnace extends TileEntity implements ISidedInventory{
             if(this.burnTime == 0 && this.canSmelt())
             {
                 this.currentItemBurnTime = this.burnTime = getItemBurnTime(this.slots[1]);
+
+                if(this.burnTime > 0)
+                {
+                    flag1 = true;
+                    if(this.slots[1] != null)
+                    {
+                        this.slots[1].stackSize--;
+                        if(this.slots[1].stackSize == 0)
+                        {
+                            this.slots[1] = this.slots[1].getItem().getContainerItem(this.slots[1]);
+                        }
+                    }
+                }
+            }
+
+            if(this.isBurning() && this.canSmelt())
+            {
+                this.cookTime++;
+
+                if(this.cookTime == this.furnaceSpeed)
+                {
+                    this.cookTime = 0;
+                    this.smeltItem();
+                    flag1 = true;
+                }
+            } else {
+                this.cookTime = 0;
+            }
+            if(flag != this.burnTime > 0)
+            {
+                flag1 = true;
+                BlockSmeltingFurnace.updateSmeltingFurnaceBlockState(this.burnTime > 0, this.worldObj, this.xCoord, this.yCoord, this.zCoord);
+            }
+        }
+        if(flag1)
+        {
+            this.markDirty();
+        }
+    }
+
+    private boolean canSmelt()
+    {
+        if(this.slots[0] == null)
+        {
+            return false;
+        }
+        else
+        {
+            ItemStack itemStack = FurnaceRecipes.smelting().getSmeltingResult(this.slots[0]);
+
+            if(itemStack == null) return  false;
+            if(this.slots[2] == null) return true;
+            if(!this.slots[2].isItemEqual(itemStack)) return false;
+
+            int result = this.slots[2].stackSize + itemStack.stackSize;
+
+            return (result <= getInventoryStackLimit() && result <= itemStack.getMaxStackSize());
+        }
+    }
+
+    public void smeltItem(){
+        if(this.canSmelt())
+        {
+            ItemStack itemStack = FurnaceRecipes.smelting().getSmeltingResult(this.slots[0]);
+
+            if(this.slots[2] == null)
+            {
+                this.slots[2] = itemStack.copy();
+            }
+            else  if(this.slots[2].isItemEqual(itemStack))
+            {
+                this.slots[2].stackSize += itemStack.stackSize;
+            }
+
+            this.slots[0].stackSize --;
+
+            if(this.slots[0].stackSize <= 0)
+            {
+                this.slots[0] = null;
             }
         }
     }
