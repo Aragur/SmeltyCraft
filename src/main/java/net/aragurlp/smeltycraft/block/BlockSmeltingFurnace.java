c2ee6003fc2d1fe82d4f00840dde5bc6130f21cf
AragurLP <aragurlp@gmail.com>
2015-04-06 01:42:07 +0200

    Added Particles

--- src/main/java/net/aragurlp/smeltycraft/block/BlockSmeltingFurnace.java
+++ src/main/java/net/aragurlp/smeltycraft/block/BlockSmeltingFurnace.java
@@ -12,11 +12,13 @@ import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.util.MathHelper;
@@ -27,11 +29,15 @@ import java.util.Random;
 
 public class BlockSmeltingFurnace extends BlockContainerSC
 {
+    private Random random = new Random();
+
     private boolean isActive;
     @SideOnly(Side.CLIENT)
     private IIcon iconFront;
     @SideOnly(Side.CLIENT)
     private IIcon iconTop;
+    @SideOnly(Side.CLIENT)
+    private IIcon iconBottom;
 
     private static boolean keepInventory;
 
@@ -50,7 +56,8 @@ public class BlockSmeltingFurnace extends BlockContainerSC
     {
         this.blockIcon = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + "smeltingfurnace_side");
         this.iconFront = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + "smeltingfurnace_front_" + (this.isActive ? "on" : "off"));
-        this.iconTop = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + "smeltingfurnace_top");
+        this.iconTop = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + "smeltingfurnace_top_" + (this.isActive ? "on" : "off"));
+        this.iconBottom = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + "smeltingfurnace_bottom");
     }
 
     @SideOnly(Side.CLIENT)
@@ -60,7 +67,11 @@ public class BlockSmeltingFurnace extends BlockContainerSC
         {
             return this.iconFront;
         }
-        if(side <= 1)
+        else if(side == 0)
+        {
+            return this.iconBottom;
+        }
+        else if(side == 1)
         {
             return this.iconTop;
         }
@@ -71,6 +82,52 @@ public class BlockSmeltingFurnace extends BlockContainerSC
         else
         {
             return this.blockIcon;
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void randomDisplayTick(World world, int x, int y, int z, Random random)
+    {
+        if(this.isActive)
+        {
+            int direction = world.getBlockMetadata(x, y, z);
+
+            float x1 = (float)x + 0.5F;
+            float y1 = (float)y + random.nextFloat() / 2.1F;
+            float z1 = (float)z + 0.5F;
+
+            float f = 0.52F;
+            float f1 = random.nextFloat() * 0.6F - 0.3F;
+
+            if(direction == 4)
+            {
+                world.spawnParticle("smoke", (double)(x1 - f), (double)y1, (double)(z1 + f1), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("smoke", (double)(x1 - f), (double)y1, (double)(z1 + f1), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(x1 - f), (double)y1, (double)(z1 + f1), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(x1 - f), (double)y1, (double)(z1 + f1), 0.0D, 0.0D, 0.0D);
+            }
+            else if(direction == 5)
+            {
+                world.spawnParticle("smoke", (double)(x1 + f), (double)y1, (double)(z1 + f1), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("smoke", (double)(x1 + f), (double)y1, (double)(z1 + f1), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(x1 + f), (double)y1, (double)(z1 + f1), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(x1 + f), (double)y1, (double)(z1 + f1), 0.0D, 0.0D, 0.0D);
+            }
+            else if(direction == 2)
+            {
+                world.spawnParticle("smoke", (double)(x1 + f1), (double)y1, (double)(z1 - f), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("smoke", (double)(x1 + f1), (double)y1, (double)(z1 - f), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(x1 + f1), (double)y1, (double)(z1 - f), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(x1 + f1), (double)y1, (double)(z1 - f), 0.0D, 0.0D, 0.0D);
+            }
+            else if(direction == 3)
+            {
+                world.spawnParticle("smoke", (double)(x1 + f1), (double)y1, (double)(z1 + f), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("smoke", (double)(x1 + f1), (double)y1, (double)(z1 + f), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(x1 + f1), (double)y1, (double)(z1 + f), 0.0D, 0.0D, 0.0D);
+                world.spawnParticle("flame", (double)(x1 + f1), (double)y1, (double)(z1 + f), 0.0D, 0.0D, 0.0D);
+            }
+
         }
     }
 
@@ -184,10 +241,63 @@ public class BlockSmeltingFurnace extends BlockContainerSC
         }
     }
 
+    public void breakBlock(World world, int x, int y, int z, Block oldBlock, int oldMetadata)
+    {
+        if(!keepInventory)
+        {
+            TileEntitySmeltingFurnace tileEntity = (TileEntitySmeltingFurnace) world.getTileEntity(x, y, z);
+
+            if(tileEntity != null)
+            {
+                for(int i = 0; i < tileEntity.getSizeInventory(); i++)
+                {
+                    ItemStack itemStack = tileEntity.getStackInSlot(i);
+
+                    if(itemStack != null)
+                    {
+                        float f = this.random.nextFloat() * 0.8F + 0.1F;
+                        float f1 = this.random.nextFloat() * 0.8F + 0.1F;
+                        float f2 = this.random.nextFloat() * 0.8F + 0.1F;
+
+                        while (itemStack.stackSize > 0)
+                        {
+                            int j = this.random.nextInt(21) + 10;
+
+                            if(j > itemStack.stackSize)
+                            {
+                                j = itemStack.stackSize;
+                            }
+
+                            itemStack.stackSize -= j;
+
+                            EntityItem item = new EntityItem(world, (double)((float)x + f), (double)((float)y + f1), (double) z + f2, new ItemStack(itemStack.getItem(), j, itemStack.getItemDamage()));
+
+                            if(itemStack.hasTagCompound())
+                            {
+                                item.getEntityItem().setTagCompound((NBTTagCompound)itemStack.getTagCompound().copy());
+                            }
+
+                            float f3 = 0.05F;
+                            item.motionX = (double) ((float)this.random.nextGaussian() * f3);
+                            item.motionY = (double) ((float)this.random.nextGaussian() * f3 + 0.2F);
+                            item.motionZ = (double) ((float)this.random.nextGaussian() * f3);
+
+                            world.spawnEntityInWorld(item);
+                        }
+                    }
+                }
+//                world.setBlock(x, y, z, oldBlock);
+            }
+        }
+
+        super.breakBlock(world, x,y, z, oldBlock, oldMetadata);
+    }
+
     public boolean hasComparatorInputOverride()
     {
         return true;
     }
+
 
     public int getComparatorInputOverride(World world, int x, int y, int z, int i)
     {
@@ -196,5 +306,14 @@ public class BlockSmeltingFurnace extends BlockContainerSC
 
     public ItemStack getPickBlock(MovingObjectPosition target, World world, int x, int y , int z, EntityPlayer player){
         return new ItemStack(ModBlocks.smeltingfurnace);
+    }
+
+    public static void doParticles(World world, int x, int y, int z, Random random) {
+        BlockSmeltingFurnace block = (BlockSmeltingFurnace) world.getBlock(x, y, z);
+        if(block.isActive){
+            for (int i = 0; i < 3; i++) {
+                world.spawnParticle("smoke", x + 0.5D, y + 0.99D, z + 0.5D, random.nextDouble() / 10 - 0.05D, 0.15D, random.nextDouble() / 10 - 0.05D);
+            }
+        }
     }
 }
