de32f9296ab9a01dc72226f0356148c6fcb1d072
AragurLP <aragurlp@gmail.com>
2015-04-05 21:51:40 +0200

    Added Shift-Click

--- src/main/java/net/aragurlp/smeltycraft/container/ContainerSmeltingFurnace.java
+++ src/main/java/net/aragurlp/smeltycraft/container/ContainerSmeltingFurnace.java
@@ -10,6 +10,7 @@ import net.minecraft.inventory.ICrafting;
 import net.minecraft.inventory.Slot;
 import net.minecraft.inventory.SlotFurnace;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.FurnaceRecipes;
 
 public class ContainerSmeltingFurnace extends Container
 {
@@ -85,9 +86,77 @@ public class ContainerSmeltingFurnace extends Container
         if(slot == 2) this.smeltingFurnace.currentItemBurnTime = newValue;
     }
 
-    public ItemStack transferStackInSlot(EntityPlayer player, int slot)
+    public ItemStack transferStackInSlot(EntityPlayer player, int clickedSlotNumber)
     {
-        return null;
+        ItemStack itemStack = null;
+        Slot slot = (Slot)this.inventorySlots.get(clickedSlotNumber);
+
+        if(slot != null && slot.getHasStack())
+        {
+            ItemStack itemStack1 = slot.getStack();
+            itemStack = itemStack1.copy();
+
+            if(clickedSlotNumber == 2)
+            {
+                if(!this.mergeItemStack(itemStack1, 3, 39, true))
+                {
+                    return null;
+                }
+
+                slot.onSlotChange(itemStack1, itemStack);
+            }
+            else if(clickedSlotNumber != 1 && clickedSlotNumber != 0)
+            {
+                if(FurnaceRecipes.smelting().getSmeltingResult(itemStack1) != null)
+                {
+                    if(!this.mergeItemStack(itemStack1, 0, 1, false))
+                    {
+                        return null;
+                    }
+                }
+                else if(TileSmeltingFurnace.isItemFuel(itemStack1))
+                {
+                    if(!this.mergeItemStack(itemStack1, 1, 2, false))
+                    {
+                        return null;
+                    }
+                }
+                else if(clickedSlotNumber >= 3 && clickedSlotNumber < 30)
+                {
+                    if(!this.mergeItemStack(itemStack1, 30, 39, false))
+                    {
+                        return null;
+                    }
+                }
+                else if(clickedSlotNumber >= 30 && clickedSlotNumber < 39)
+                {
+                    if(!this.mergeItemStack(itemStack1, 3, 30, false))
+                    {
+                        return null;
+                    }
+                }
+            }
+            else if(!this.mergeItemStack(itemStack1, 3, 39, false))
+            {
+                return null;
+            }
+
+            if(itemStack1.stackSize == 0)
+            {
+                slot.putStack(null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+            if(itemStack1.stackSize == itemStack.stackSize)
+            {
+                return null;
+            }
+            slot.onPickupFromSlot(player, itemStack1);
+        }
+
+        return itemStack;
     }
 
     @Override
